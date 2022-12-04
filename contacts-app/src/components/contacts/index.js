ab84c2d3a10b12ba19ef1d3ae90e142fec97fafa
argun8394 <argundoganali@gmail.com>
2022-12-04 23:57:44 +0300

    contacts app

---
+++ contacts-app/src/components/contacts/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './form'
+import List from './List/'
+
+function Contacts() {
+  return (
+    <div>
+        <List/>
+        <Form/>
+    </div>
+  )
+}
+
+export default Contacts
