03bbdfdcd084eb59a84ea3dc444d2ef762a4ee68
argun8394 <argundoganali@gmail.com>
2022-12-06 22:10:47 +0300

    form component

--- contacts-app/src/components/contacts/form/index.js
+++ contacts-app/src/components/contacts/form/index.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
-function Form() {
+const initialFormValues = {fullname:"", phone_number:""}
+function Form({addContact, contacts}) {
+  // console.log(addContact);
+
+  const [form, setForm]=useState(initialFormValues);
+
+  useEffect(()=>{ 
+    setForm(initialFormValues);
+  }, [contacts]) //contacts teki değişikliği useEffect ile yakalayarak form resetleme 
+
+  const onChangeInput = (e) => {
+    setForm({...form, [e.target.name]:e.target.value})
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();// form elementinin varsayılan davranışı olan veriyi bir endpoint e aktarıp sayfa yenileme işlemi yapar 
+    //onSubmit anında bu varsayılan davranışı önlemek için e.preventDefault() yöntemi kullanılır
+
+    if(form.fullname === "" || form.phone_number === "") {
+      return false;
+    }
+
+    addContact([...contacts, form]);
+    // console.log(form)
+
+    // setForm({fullname:"", phone_number:"",}) //form resetleme -- 1
+    // setForm(initialFormValues) //form resetleme // form resetleme -- 2
+
+  }
   return (
-    <div>Form</div>
+    <form onSubmit={onSubmit}>
+      <div>
+        <input name="fullname" placeholder="Full Name" 
+        onChange={onChangeInput}
+        value={form.fullname}
+        />
+      </div>
+
+      <div>
+        <input name="phone_number" placeholder="Phone Number"
+        onChange={onChangeInput}
+        value={form.phone_number}
+        />
+      </div>
+      <div>
+        <button>Add</button>
+      </div>
+    </form>
   )
 }
 
