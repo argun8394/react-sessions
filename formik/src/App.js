9f505223847a72b02c91ee96586162ff776b02a7
argun8394 <argundoganali@gmail.com>
2022-12-14 23:49:27 +0300

    radio, checkbox, dropdown

--- formik/src/App.js
+++ formik/src/App.js
@@ -8,34 +8,77 @@ function App() {
       <h1>Sign Up</h1>
       <Formik
         initialValues={{
-          firstName: '',
-          lastName: '',
-          email: '',
+          firstName: 'ali',
+          lastName: 'argun',
+          email: 'argun23@gmail.com',
+          gender: 'male',
+          hobbies: [],
+          country: ''
         }}
         onSubmit={(values) => {
           console.log(values);
         }}
       >
         {/*handleSubmit içinde formumuzu return ederek çalıştırıyoruz */}
-         {({handleSubmit, handleChange}) =>(   
+         {({handleSubmit, handleChange, values}) =>(   
           <form onSubmit={handleSubmit}>
           <label htmlFor="firstName">First Name</label>
-          <input name="firstName" onChange={handleChange}/> {/* handleChange kullanmadığımızda inputa girilen değer value ya yansımaz boş görünür*/}
+          <input name="firstName" onChange={handleChange} value={values.firstName}/> {/* handleChange kullanmadığımızda inputa girilen değer value ya yansımaz boş görünür*/}
            {/*name initialValues taki firstName verilmelidir  */}
+           {/*value={values.firstName} initialValue daki değerin yansıtılmasını sağlar*/}
           <br />
           <br />
 
           <label htmlFor="lastName">Last Name</label>
-          <input name="lastName" onChange={handleChange}/>
+          <input name="lastName" onChange={handleChange} value={values.lastName}/>
           <br />
           <br />
 
           <label htmlFor="email">Email</label>
-          <input name="email" onChange={handleChange}/>
+          <input name="email" onChange={handleChange} value={values.email}/>
+          <br />
+          <br />
+
+          <span>Male</span>
+          <input type="radio" name="gender" value="male" onChange={handleChange} checked={values.gender === 'male'}/> {/*checked={values.gender === 'male'} initialValue da verilen değerin işlenmiş halde gelmesini sağlar */}
+
+          <span>Female</span>
+          <input type="radio" name="gender" value="female" onChange={handleChange} checked={values.gender === 'female'}/>
+
+          <br />
+          <br />
+
+          <div>
+            <input type="checkbox" name="hobbies" value="Football"  onChange={handleChange}/>
+            Football
+          </div>
+          <div>
+            <input type="checkbox" name="hobbies" value="Cinema"  onChange={handleChange}/>
+            Football
+          </div>
+          <div>
+            <input type="checkbox" name="hobbies" value="Travel"  onChange={handleChange}/>
+            Travel
+          </div>
+
+          <br />
+          <br />
+
+          <select name="country" value={values.country} onChange={handleChange}>
+            <option value="turkey">Turkey</option>
+            <option value="england">England</option>
+            <option value="usa">USA</option>
+          </select>
+
           <br />
           <br />
 
           <button type="submit">Submit</button>
+          <br />
+          <br />
+
+          {JSON.stringify(values)}
+
         </form>
 )} 
       </Formik>
