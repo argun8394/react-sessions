4e8bf7865c3f963233ac230cd3c5e67d663c837a
argun8394 <argundoganali@gmail.com>
2022-12-14 22:45:43 +0300

    formik

---
+++ formik/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import { Formik, Field, Form } from 'formik';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" placeholder="Jane" />  {/*name initialValues taki firstName verilmelidir  */}
+          <br />
+          <br />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field name="lastName" placeholder="Doe" />
+          <br />
+          <br />
+
+          <label htmlFor="email">Email</label>
+          <Field
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+          <br />
+          <br />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default App;
