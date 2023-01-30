afcf02dbd400a2f17e328c151b47ea3f1fe2a756
argun8394 <argundoganali@gmail.com>
2023-01-30 12:37:22 +0300

    useMemo

--- memoization/src/App.js
+++ memoization/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useMemo} from 'react';
 import Header from './components/Header'
+
+// const data = {name: 'John'} dışarda tanımlanırsa değeri değişmediği sürece re-render edilmez (veya useMemo() ile içerde kullanmak ta gereksiz  re-render ı önler)
 
 function App() {
   const [number, setNumber] = useState(0);
+  //buton tıklandığında burada bulunan herşeyin render edilmesine sebep olur React Memoization yöntemlerini kullanarak sadece 
+  //ilgili alan re-render edilmiş olur
+
+ /* const data = {name: 'John'} //bu şekilde kullandığımızda buradaki data Header da prop olarak geçtiği için değer değişmese bile farklı referansa sahip olduğu için her defasında Header da re-render edilir.
+ */
+  const data = useMemo(()=>{
+    return {name: 'John', number}
+    }, [number]) //dependency array de verceğimiz veriler değiştiğinde data da yeniden hesaplanır [burada number her değiştiğinde data yeniden hesaplanır ve Header comp de re-render edilir]
+    //useMemo kullanmadığımızda component içinde bir input olduğunu varsayrsak inputa her veri girişi veya tıklamada yeniden hesaplama yaparak gerksiz render a sebep olur
   return (
     <div className="App">
-      <Header />
+      {/* number = {number <5 ? 0 : number } number 5 ve üstü olunca header da re-render edilir */}
+      <Header data={data} />{/*burada geçen bir prop güncellendiğinde render edilir */} 
 
     <hr/>
 
@@ -18,3 +30,15 @@ function App() {
 }
 
 export default App;
+
+/*Aşağıdaki denklik durumundan dolayı obje geçtiğimizde her defasında farklı referans algılayıp değer değişşmese bile re-render a sebep olur
+Burada her tıklamada bu component re-render edilir ve const data = {name: 'John'} satırına geldiğinde denklik kontrolü yapıp farklı olduğunu görüp re-render eder.
+// 5 === 5 true
+// " " === " " true
+// true === true true 
+// yukarıdaki denklikleri sorguladığımızda true değeri döner primitive veri türlerinde bu şekilde
+//----------------------------------------------------------------
+// Ama obje ve array denkliğini sorguladığımızda bize bellekteki referansları farklı olduğu için false döner
+// {} === {} false
+// [] === [] false
+*/
--- memoization/src/components/Header.js
+++ memoization/src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
-function Header() {
-
+function Header({number, data}) {
+//Appjs teki butona her tıklamada Header comp  de re render olur (React.memo kullanarak bunu önlemiş olduk)
   console.log('Header component re-rendered!')
 
   return (
-    <div>Headr</div>
+    <div>
+      Headr - {number} 
+      <br/>
+      <br/>
+    <code>{JSON.stringify(data)}</code>
+
+    </div>
+    
   )
 }
 
-export default React.memo(Header);//Bu şekilde export ederek gereksiz re-render edilmesini önleriz
+export default React.memo(Header);//Bu şekilde export ederek gereksiz re-render edilmesini önleriz ve componentlerimiz daha performanslı hale getirmiş oluruz
 //export default Header// şeklinde export edersek number değerini her değişiklikte Headerda değişiklik olmamasına rağmen gereksiz yere"""re-render""" ediliyor.
